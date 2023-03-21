e3d7909d3fc2a3d2a96bab8c394d0953e7347194
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-03-21 23:29:21 +0900

    loginActivity 추가 ActionBar 제거

---
+++ app/src/main/java/org/techtown/kormate/LoginActivity.kt
@@ -0,0 +1,16 @@
+package org.techtown.kormate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+
+    }
+}
--- app/src/main/java/org/techtown/kormate/SplashActivity.kt
+++ app/src/main/java/org/techtown/kormate/SplashActivity.kt
@@ -1,7 +1,9 @@
 package org.techtown.kormate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,22 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
 
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
 
     }
+    companion object {
+        private const val DURATION : Long = 2000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+
+
 }
