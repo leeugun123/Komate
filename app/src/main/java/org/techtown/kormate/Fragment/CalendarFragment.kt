acaaa926d7da449fbb29375b04ba505c81e3c11b
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-03-22 00:17:30 +0900

    BottomNavigationView 구성

---
+++ app/src/main/java/org/techtown/kormate/Fragment/CalendarFragment.kt
@@ -0,0 +1,23 @@
+package org.techtown.kormate.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.techtown.kormate.R
+
+
+class CalendarFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+
+
+}
--- app/src/main/java/org/techtown/kormate/MainActivity.kt
+++ app/src/main/java/org/techtown/kormate/MainActivity.kt
@@ -2,12 +2,41 @@ package org.techtown.kormate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.techtown.kormate.Fragment.BoardFragment
+import org.techtown.kormate.Fragment.CalendarFragment
+import org.techtown.kormate.Fragment.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        supportFragmentManager.beginTransaction().add(R.id.linearLayout,HomeFragment()).commit()
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.page_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_board -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, BoardFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_my -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }
+
+
+        return false
     }
 }
