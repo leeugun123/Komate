3b73d85185e547b32f374d8294fc598a943890e8
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-03-25 01:00:43 +0900

    게시판 data class 생성

---
+++ app/src/main/java/org/techtown/kormate/Fragment/BoardDetail.kt
@@ -0,0 +1,7 @@
+package org.techtown.kormate.Fragment
+
+data class BoardDetail(val userName : String,
+                       val userImg : Int,
+                       val date : String ,
+                       val time : String,
+                       val post : String)
---
+++ app/src/main/java/org/techtown/kormate/Fragment/BoardPreview.kt
@@ -0,0 +1,3 @@
+package org.techtown.kormate.Fragment
+
+data class BoardPreview(val date : String,val time : String,val post : String)
