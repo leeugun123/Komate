8fedc9f60571c83272eebc75b6f5e2349e272ee1
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-04-18 17:25:55 +0900

    Comment 클래스 Parcelable 적용

--- app/src/main/java/org/techtown/kormate/Fragment/Data/Comment.kt
+++ app/src/main/java/org/techtown/kormate/Fragment/Data/Comment.kt
@@ -1,12 +1,42 @@
 package org.techtown.kormate.Fragment.Data
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Comment(
+    var userName: String? = null,
+    var userImg: String? = null,
+    var text: String? = null,
+    var createdTime: String? = null
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
 
-    val userName : String? = null,
-    val userImg : String? = null,
-    val text: String? = null,
-    val createdTime: String? = null
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(userName)
+        parcel.writeString(userImg)
+        parcel.writeString(text)
+        parcel.writeString(createdTime)
+    }
 
-)
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Comment> {
+        override fun createFromParcel(parcel: Parcel): Comment {
+            return Comment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Comment?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
 
 
