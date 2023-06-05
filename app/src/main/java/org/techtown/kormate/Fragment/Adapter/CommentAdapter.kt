12999263f0ef21553f0a268737ff2b826d810015
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-06-06 00:36:26 +0900

    댓글 신고 기능 구현

--- app/src/main/java/org/techtown/kormate/Fragment/Adapter/CommentAdapter.kt
+++ app/src/main/java/org/techtown/kormate/Fragment/Adapter/CommentAdapter.kt
@@ -2,6 +2,7 @@ package org.techtown.kormate.Fragment.Adapter
 
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -24,10 +25,13 @@ import com.kakao.sdk.user.UserApiClient
 import org.techtown.kormate.CurrentDateTime
 import org.techtown.kormate.Fragment.Data.BoardDetail
 import org.techtown.kormate.Fragment.Data.Comment
+import org.techtown.kormate.Fragment.Data.Report
+import org.techtown.kormate.R
+import org.techtown.kormate.R.drawable.ic_baseline_report_24
 import org.techtown.kormate.databinding.CommentimgBinding
 
 
-class CommentAdapter(private val comments : MutableList<Comment>, private val userId : Long, private val postId : String) :
+class CommentAdapter(private val comments : MutableList<Comment>, private var userId : Long, private val postId : String) :
     RecyclerView.Adapter<CommentAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentAdapter.ViewHolder {
@@ -49,7 +53,7 @@ class CommentAdapter(private val comments : MutableList<Comment>, private val us
 
         RecyclerView.ViewHolder(binding.root){
 
-            val deleteButton : ImageButton = binding.deleteButton
+            val button : ImageButton = binding.Button
 
             fun bind(comment : Comment){
 
@@ -66,7 +70,7 @@ class CommentAdapter(private val comments : MutableList<Comment>, private val us
 
                 if(userId == comment.userId){
 
-                    deleteButton.setOnClickListener {
+                    button.setOnClickListener {
 
                         val builder = AlertDialog.Builder(binding.root.context)
                         builder.setTitle("댓글을 삭제하시겠습니까?")
@@ -88,13 +92,57 @@ class CommentAdapter(private val comments : MutableList<Comment>, private val us
 
                         builder.create().show()
 
-
                     }
-
 
                 }
                 else{
-                    deleteButton.visibility = View.GONE
+
+                    button.setImageResource(ic_baseline_report_24)
+
+                    button.setOnClickListener {
+
+                        val reasons = arrayOf("욕설", "도배", "인종 혐오 표현", "성적인 만남 유도")
+                        val checkedReasons = booleanArrayOf(false, false, false, false, false)
+
+                        val builder = AlertDialog.Builder(binding.root.context)
+
+                        builder.setTitle("신고 사유를 선택하세요")
+                        builder.setMultiChoiceItems(reasons, checkedReasons) { _, which, isChecked ->
+                            checkedReasons[which] = isChecked
+                        }
+
+                        builder.setPositiveButton("확인") { _, _ ->
+
+                            val selectedReasons = mutableListOf<String>()
+
+                            for (i in reasons.indices) {
+
+                                if (checkedReasons[i]) {
+                                    selectedReasons.add(reasons[i])
+                                }
+
+                            }
+
+                            //선택한 신고 사유들에 대한 처리 진행
+                            Firebase.database.reference.child("commentsReports").child(Firebase.database.reference.push().key.toString()).setValue(
+                                Report(userId,selectedReasons,comment?.userId,comment?.id)
+                            )
+                            //신고 넣기
+
+                            Toast.makeText(context, "신고가 접수 되었습니다.", Toast.LENGTH_SHORT).show()
+
+                        }
+
+                        builder.setNegativeButton("취소") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+
+                        val dialog = builder.create()
+                        dialog.show()
+
+
+                    }
+
                 }
 
 
@@ -102,9 +150,8 @@ class CommentAdapter(private val comments : MutableList<Comment>, private val us
 
             }
 
-
-
-
         }
+
+
 
 }
