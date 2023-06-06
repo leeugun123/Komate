f0a198e52402681af7210122f8b7e5f8385825d3
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-06-06 18:45:21 +0900

    내 정보 xml 구성

--- app/src/main/java/org/techtown/kormate/Fragment/MyFragment.kt
+++ app/src/main/java/org/techtown/kormate/Fragment/MyFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.kakao.sdk.user.UserApiClient
 import org.techtown.kormate.R
 import org.techtown.kormate.databinding.FragmentMyBinding
 
@@ -17,6 +19,22 @@ class MyFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         binding = FragmentMyBinding.inflate(layoutInflater)
+
+        UserApiClient.instance.me { user, error ->
+
+            "${user?.kakaoAccount?.profile?.nickname}".also {
+
+                if(it != null)
+                    binding!!.userName.text = it
+
+            }
+
+            if(user?.kakaoAccount?.profile?.profileImageUrl != null)
+                Glide.with(binding!!.userProfile).load(user?.kakaoAccount?.profile?.profileImageUrl).circleCrop().into(binding!!.userProfile)
+
+
+        }//내 정보 카카오 oAuth로 가져오기
+
 
 
 
