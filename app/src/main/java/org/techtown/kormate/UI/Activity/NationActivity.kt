cda691cdb6af518d52b744e49f61fafdfbd365ff
leeugun123 <leeugun123@naver.com>
2024-02-03 14:59:59 +0900

    클린 코드 적용

--- app/src/main/java/org/techtown/kormate/UI/Activity/NationActivity.kt
+++ app/src/main/java/org/techtown/kormate/UI/Activity/NationActivity.kt
@@ -11,86 +11,68 @@ import org.techtown.kormate.databinding.ActivityNationBinding
 
 class NationActivity : AppCompatActivity() {
 
-    private var binding : ActivityNationBinding? = null
-
+    private val binding by lazy { ActivityNationBinding.inflate(layoutInflater)}
+    private lateinit var nation : String
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding = ActivityNationBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(binding.root)
 
-        var nation : String = ""
-
-
-
-        binding!!.radioGroup.setOnCheckedChangeListener { group, checkId ->
-
-            when(checkId) {
-
-                binding!!.radio1.id -> {
-
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                    nation = "한국"
-
-                }//한국
-
-                binding!!.radio2.id -> {
-
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                    nation = "중국"
-
-
-                }//중국
-
-                binding!!.radio3.id -> {
-
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                    nation = "베트남"
-
-
-                }//베트남
-
-                binding!!.radio4.id -> {
-
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                    nation = "몽골"
-
-
-                }//몽골
-
-                binding!!.radio5.id -> {
-
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                    nation = "우즈베키스탄"
-
-                }//우즈베키스탄
-
-            }
-
+        binding.radioGroup.setOnCheckedChangeListener { _, checkId ->
+            radioCheck(checkId)
         }
 
-        binding!!.checkButton.setOnClickListener {
-
-            if(nation == ""){
-                Toast.makeText(this,"국가를 체크해주세요",Toast.LENGTH_SHORT).show()
-            }
-            else{
-
-                //서버 입력 작업 처리
-                var userIntel : UserIntel = UserIntel(nation,"","","")
-
-                val intent = Intent(this, MajorActivity::class.java)
-                intent.putExtra("userIntel",userIntel)
-                startActivity(intent)
-
-                finish()
-
-            }
-
-
+        binding.checkButton.setOnClickListener {
+            nationCheck()
         }
 
 
+    }
+
+    private fun nationCheck() {
+        if(nation == "")
+            Toast.makeText(this,"국가를 체크해주세요",Toast.LENGTH_SHORT).show()
+        else
+            moveToMajorActivity()
+    }
+
+    private fun moveToMajorActivity() {
+        val intent = Intent(this, MajorActivity::class.java)
+        intent.putExtra("userIntel", UserIntel(nation,"","",""))
+        startActivity(intent)
+        finish()
+    }
+
+    private fun radioCheck(checkId : Int) {
+
+        when(checkId) {
+
+            binding.radio1.id -> {
+                binding.checkButton.setBackgroundResource(R.color.blue)
+                nation = "한국"
+            }
+
+            binding.radio2.id -> {
+                binding.checkButton.setBackgroundResource(R.color.blue)
+                nation = "중국"
+            }
+
+            binding.radio3.id -> {
+                binding.checkButton.setBackgroundResource(R.color.blue)
+                nation = "베트남"
+            }
+
+            binding.radio4.id -> {
+                binding!!.checkButton.setBackgroundResource(R.color.blue)
+                nation = "몽골"
+            }
+
+            binding.radio5.id -> {
+                binding.checkButton.setBackgroundResource(R.color.blue)
+                nation = "우즈베키스탄"
+            }
+
+        }
     }
 
 }
--- app/src/main/java/org/techtown/kormate/UI/Activity/SelfIntroActivity.kt
+++ app/src/main/java/org/techtown/kormate/UI/Activity/SelfIntroActivity.kt
@@ -15,82 +15,69 @@ import org.techtown.kormate.databinding.ActivitySelfIntroBinding
 
 class SelfIntroActivity : AppCompatActivity() {
 
-    private var binding : ActivitySelfIntroBinding? = null
-
-    private lateinit var kakaoViewModel : KakaoViewModel
-
+    private val binding by lazy { ActivitySelfIntroBinding.inflate(layoutInflater) }
+    private val kakaoViewModel by lazy { ViewModelProvider(this).get(KakaoViewModel::class.java) }
+    private val receivedIntel by lazy { intent.getParcelableExtra<UserIntel>("userIntel") }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySelfIntroBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(binding.root)
 
-        kakaoViewModel = ViewModelProvider(this).get(KakaoViewModel::class.java)
-        kakaoViewModel.loadUserData()
+        kakaoViewUiSync()
 
-        kakaoViewModel.userName.observe(this){ userName ->
-            binding!!.userName.text = userName.toString()
-        }
+        binding.selfEdittext.addTextChangedListener(object : TextWatcher {
 
-        kakaoViewModel.userProfileImageUrl.observe(this){ userImg ->
-            Glide.with(binding!!.userpic).load(userImg.toString()).circleCrop().into(binding!!.userpic)
-        }
-
-
-
-        var receivedIntel  = intent.getParcelableExtra<UserIntel>("userIntel")
-
-
-        binding!!.selfEdittext.addTextChangedListener(object : TextWatcher {
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // 입력 전에 호출되는 메서드
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            // 입력 전에 호출되는 메서드
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding!!.checkButton.setBackgroundResource(R.color.blue)
+                binding.checkButton.setBackgroundResource(R.color.blue)
             }
 
             override fun afterTextChanged(s: Editable?) {
 
                 val userInput = s?.toString()?.trim() // 사용자의 입력값
 
-                if (userInput?.length ?: 0 >= 1) {
-                    // 입력한 글자가 1글자 이상인 경우 버튼의 색상 변경
-                    binding!!.checkButton.setBackgroundResource(R.color.blue)
-                } else {
-                    // 입력한 글자가 없는 경우 버튼의 색상 초기화
-                    binding!!.checkButton.setBackgroundResource(R.color.gray)
-                }
-
+                if ((userInput?.length ?: 0) >= 1)
+                    binding!!.checkButton.setBackgroundResource(R.color.blue) // 입력한 글자가 1글자 이상인 경우 버튼의 색상 변경
+                else
+                    binding!!.checkButton.setBackgroundResource(R.color.gray) // 입력한 글자가 없는 경우 버튼의 색상 초기화
             }
-
 
         })
 
-
-        binding!!.checkButton.setOnClickListener {
-
-            if(binding!!.selfEdittext.text.toString().isNotEmpty()){
-
-
-                val intent = Intent(this, GenderActivity::class.java)
-
-                receivedIntel!!.selfIntro = binding!!.selfEdittext.text.toString()
-
-                intent.putExtra("userIntel",receivedIntel)
-
-                startActivity(intent)
-                finish()
-
-            }
-            else{
-                Toast.makeText(this,"자기소개를 작성해주세요.", Toast.LENGTH_SHORT).show()
-            }
-
-
+        binding.checkButton.setOnClickListener {
+            checkSelfEditText()
         }
 
+    }
 
+    private fun checkSelfEditText() {
+
+        if(binding.selfEdittext.text.toString().isNotEmpty())
+            moveToGenderActivity()
+        else
+            Toast.makeText(this,"자기소개를 작성해주세요.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun moveToGenderActivity() {
+        val intent = Intent(this, GenderActivity::class.java)
+        receivedIntel!!.selfIntro = binding.selfEdittext.text.toString()
+        intent.putExtra("userIntel",receivedIntel)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun kakaoViewUiSync() {
+
+        kakaoViewModel.loadUserData()
+
+        kakaoViewModel.userName.observe(this){ userName ->
+            binding.userName.text = userName.toString()
+        }
+
+        kakaoViewModel.userProfileImageUrl.observe(this){ userImg ->
+            Glide.with(binding.userpic).load(userImg.toString()).circleCrop().into(binding.userpic)
+        }
 
     }
 
