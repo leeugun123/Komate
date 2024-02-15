a5ac5dcc4fcc1ec55bbd0a5a0ffe50aff0556e65
leeugun123 <leeugun123@naver.com>
2024-02-15 21:03:32 +0900

    uiBinding 적용

--- app/src/main/java/org/techtown/kormate/UI/Fragment/HomeFragment.kt
+++ app/src/main/java/org/techtown/kormate/UI/Fragment/HomeFragment.kt
@@ -1,6 +1,7 @@
 package org.techtown.kormate.UI.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,12 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.techtown.kormate.Model.UserKakaoIntel
+import org.techtown.kormate.Model.UserKakaoIntel.userNickName
+import org.techtown.kormate.Model.UserKakaoIntel.userProfileImg
 import org.techtown.kormate.UI.Adapter.RecentAdapter
 import org.techtown.kormate.UI.ViewModel.RecentListViewModel
 import org.techtown.kormate.databinding.FragmentHomeBinding
@@ -29,16 +34,36 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        uiBinding()
         recentListObserve()
+    }
+
+    private fun uiBinding() {
+
+        profileImgBinding()
+        nameBinding()
+    }
+
+    private fun nameBinding() {
+        binding.userName.text = userNickName
+    }
+
+    private fun profileImgBinding() {
+
+        Glide.with(requireActivity())
+            .load(userProfileImg)
+            .circleCrop()
+            .into(binding.userProfile)
+
     }
 
     private fun recentListObserve() {
 
-        lifecycleScope.launch(Dispatchers.Main) {
-            recentListViewModel.loadRecentData(true) //limit 개수만큼 가져옴
-        }
+
+        recentListViewModel.loadRecentData(true) //limit 개수만큼 가져옴
 
         recentListViewModel.recentList.observe(viewLifecycleOwner) { recentList ->
+            Log.e("TAG",recentList.size.toString() + "  " + "사이즈의 개수")
             binding.recentRecyclerview.layoutManager = LinearLayoutManager(requireContext())
             binding.recentRecyclerview.adapter = RecentAdapter(recentList)
         }
