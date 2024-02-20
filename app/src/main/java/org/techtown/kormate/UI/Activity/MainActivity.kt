0e8d5dd676f3f12209fa1077339657335a1fb3d6
leeugun123 <leeugun123@naver.com>
2024-02-20 20:31:42 +0900

    한번 더 누르면 앱 종료 기능 구현

--- app/src/main/java/org/techtown/kormate/UI/Activity/MainActivity.kt
+++ app/src/main/java/org/techtown/kormate/UI/Activity/MainActivity.kt
@@ -2,8 +2,11 @@ package org.techtown.kormate.UI.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.techtown.kormate.UI.Fragment.BoardFragment
 import org.techtown.kormate.UI.Fragment.HomeFragment
@@ -12,6 +15,8 @@ import org.techtown.kormate.R
 import org.techtown.kormate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -31,18 +36,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun initNavigationBar() {
 
-        binding.bottomNavigationView.run {
+        binding.bottomNavigationView.setOnItemSelectedListener{ item ->
 
-            setOnItemSelectedListener {item ->
-                when(item.itemId){
-                    R.id.page_home -> { showFa() }
-                    R.id.page_board -> { showFb() }
-                    R.id.page_my -> { showFc() }
-                    else -> throw IllegalArgumentException("유효하지 않습니다.")
-                }
-                true
+            when(item.itemId){
+                R.id.page_home -> { showFa() }
+                R.id.page_board -> { showFb() }
+                R.id.page_my -> { showFc() }
+                else -> throw IllegalArgumentException("유효하지 않습니다.")
             }
-            selectedItemId = R.id.page_home
+            true
         }
 
     }
@@ -70,6 +72,29 @@ class MainActivity : AppCompatActivity() {
         fragmentManager.beginTransaction().add(R.id.main_frame, fb).commit()
         fragmentManager.beginTransaction().add(R.id.main_frame, fc).commit()
     }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, BACK_PRESS_EXIT_GUIDE, Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, BACK_PRESS_WAIT_TIME)  // 2초간 뒤로가기 버튼을 두 번 눌러야 종료되도록 설정
+
+    }
+
+    companion object{
+        private const val BACK_PRESS_EXIT_GUIDE = "한번 더 누르면 앱이 종료됩니다."
+        private const val BACK_PRESS_WAIT_TIME : Long = 2000
+    }
+
 
 
 
