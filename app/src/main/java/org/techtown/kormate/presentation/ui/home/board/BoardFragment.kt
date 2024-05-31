31131127228500a1b2083bf992459e182be5c59a
leeugun123 <leeugun123@naver.com>
2024-05-31 15:12:47 +0900

    [Ref/#3] : BoardFragment 데이터 바인딩 적용

--- app/src/main/java/org/techtown/kormate/presentation/ui/home/board/BoardFragment.kt
+++ app/src/main/java/org/techtown/kormate/presentation/ui/home/board/BoardFragment.kt
@@ -4,19 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import org.techtown.kormate.R
 import org.techtown.kormate.databinding.FragmentBoardBinding
 import org.techtown.kormate.domain.BoardDetail
 import org.techtown.kormate.presentation.BaseFragment
 import org.techtown.kormate.presentation.FragmentCallback
 import org.techtown.kormate.presentation.constant.FirebasePathConstant.POST_PATH_INTENT
-import org.techtown.kormate.presentation.constant.IntentCode.RESPONSE_CODE_BOARD_SYNC
 import org.techtown.kormate.presentation.ui.home.board.detail.BoardActivity
 import org.techtown.kormate.presentation.ui.home.board.detail.BoardViewModel
-import org.techtown.kormate.presentation.ui.home.board.detail.post.BoardPostActivity
 import org.techtown.kormate.presentation.ui.home.preview.PreviewAdapter
 
 
@@ -24,60 +20,35 @@ class BoardFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_board
     FragmentCallback {
 
     private val boardViewModel: BoardViewModel by viewModels({ requireParentFragment() })
-    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bindingApply()
-        dataUiBindingInit()
-        activityResultLauncherInit()
-    }
-
-    private fun activityResultLauncherInit() {
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                if (it.resultCode == RESPONSE_CODE_BOARD_SYNC)
-                    getBoardList()
-            }
-    }
-
-    private fun bindingApply() {
-
-        binding.apply {
-            movePost.setOnClickListener {
-                activityResultLauncher.launch(Intent(activity, BoardPostActivity::class.java))
-            }
-            boardSwipeRefresh.setOnRefreshListener {
-                getBoardList()
-            }
-        }
-    }
-
-
-    private fun dataUiBindingInit() {
+        initBinding()
         getBoardList()
-        recentListObserve()
+        observeRecentList()
     }
 
-
-    private fun recentListObserve() {
-
-        boardViewModel.boardDetailList.observe(viewLifecycleOwner) { recentList ->
-            binding.boardRecyclerview.layoutManager = LinearLayoutManager(requireContext())
-            binding.boardRecyclerview.adapter = PreviewAdapter(recentList)
-            binding.boardSwipeRefresh.isRefreshing = false
-        }
-
+    private fun initBinding() {
+        binding.onRefreshClick = ::getBoardList
+        binding.onPostClick = ::moveToPostFragment
     }
 
     private fun getBoardList() {
         boardViewModel.getBoardList()
     }
+    private fun moveToPostFragment(){
+        // TODO("BoardFragment로 이동하는 로직 구현")
+    }
 
-    override fun onNavigateToActivity(boardDetail: BoardDetail) {
-        val intent = Intent(requireActivity(), BoardActivity::class.java)
-        intent.putExtra(POST_PATH_INTENT, boardDetail)
-        activityResultLauncher.launch(intent)
+    private fun observeRecentList() {
+        boardViewModel.boardDetailList.observe(viewLifecycleOwner) { recentList ->
+            binding.boardRecyclerview.adapter = PreviewAdapter(recentList)
+            binding.boardSwipeRefresh.isRefreshing = false
+        }
+    }
+
+    override fun onNavigateToBoardFragment(boardDetail: BoardDetail) {
+        //TODO("BoardFragment로 이동하는 로직 구현")
     }
 }
 
