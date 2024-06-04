b5fbccc85140f626a749888f41ed452fd590d69a
leeugun123 <leeugun123@naver.com>
2024-06-04 14:47:05 +0900

    [Fix/#7] : ImageDetailActivity -> ImageDetailFragment 변경

--- app/src/main/java/org/techtown/kormate/presentation/ui/home/board/detail/gallery/ImageDetailFragment.kt
+++ app/src/main/java/org/techtown/kormate/presentation/ui/home/board/detail/gallery/ImageDetailFragment.kt
@@ -1,37 +1,49 @@
 package org.techtown.kormate.presentation.ui.home.board.detail.gallery
 
 import android.app.DownloadManager
-import android.app.DownloadManager.Request
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.DOWNLOAD_SERVICE
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import com.bumptech.glide.Glide
-import org.techtown.kormate.databinding.ActivityImageDetailBinding
+import org.techtown.kormate.R
+import org.techtown.kormate.databinding.FragmentImageDetailBinding
+import org.techtown.kormate.presentation.BaseFragment
 
-class ImageDetailActivity : AppCompatActivity() {
+class ImageDetailFragment :
+    BaseFragment<FragmentImageDetailBinding>(R.layout.fragment_image_detail) {
 
-    private val binding by lazy { ActivityImageDetailBinding.inflate(layoutInflater) }
-    private val receiveUrl by lazy { intent.getStringExtra("imgUrl") }
-    private val entirePage by lazy { intent.getIntExtra("entirePage", ENTIRE_PAGE_NUM) }
-    private val curPage by lazy { intent.getIntExtra("currentPage", CUR_PAGE_NUM) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+    private val receiveUrl by lazy { /*"intent.getStringExtra("imgUrl")"*/ "" }
+    private val entirePage by lazy { /* intent.getIntExtra("entirePage", ENTIRE_PAGE_NUM)*/ "" }
+    private val curPage by lazy { /* intent.getIntExtra("currentPage", CUR_PAGE_NUM) */ "" }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         uiSync()
-        bindingApply()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            bindingApply()
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun bindingApply() {
         binding.apply {
             finishButton.setOnClickListener {
-                finish()
+                //TODO("끝내기")
             }
             downButton.setOnClickListener {
                 imgDownload()
@@ -39,16 +51,17 @@ class ImageDetailActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun imgDownload() {
 
         // 이미지 다운로드 URL
-        val request = Request(Uri.parse(receiveUrl))
+        val request = DownloadManager.Request(Uri.parse(receiveUrl))
 
         // Set network types, title, and description
         request.apply {
-            setAllowedNetworkTypes(Request.NETWORK_WIFI or Request.NETWORK_MOBILE)
+            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
             setTitle("Downloading Image")
-            setDescription("Downloading ${receiveUrl?.substringAfterLast("/")}")
+            setDescription("Downloading ${receiveUrl.substringAfterLast("/")}")
         }
 
         // 이미지가 저장될 경로와 파일명을 지정합니다.
@@ -59,46 +72,39 @@ class ImageDetailActivity : AppCompatActivity() {
         request.setDestinationUri(Uri.parse("file://$filePath"))
 
         // DownloadManager를 통해 이미지를 다운로드합니다.
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val downloadManager = requireActivity().getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         val downloadId = downloadManager.enqueue(request)
 
         // 이미지 다운로드가 완료되었을 때 브로드캐스트 리시버에서 처리합니다.
         val onComplete = object : BroadcastReceiver() {
-
             override fun onReceive(context: Context?, intent: Intent?) {
-
                 // 다운로드가 완료된 파일의 ID와 현재 다운로드 ID를 비교하여 동일하면 다운로드가 완료된 것입니다.
                 if (intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1) == downloadId)
                     Toast.makeText(context, IMAGE_DOWNLOAD_COMPLETE, Toast.LENGTH_SHORT).show()
                 // 이미지를 열기 위한 인텐트를 생성합니다.
-
             }
-
         }
 
-
-        registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireActivity().registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+                Context.RECEIVER_NOT_EXPORTED)
+        }
     }
 
     private fun uiSync() {
-        binding.cur.text = curPage.toString()
-        binding.entire.text = entirePage.toString()
+        binding.cur.text = curPage
+        binding.entire.text = entirePage
 
         Glide.with(this)
             .load(receiveUrl)
             .override(IMG_WIDTH_SIZE, IMG_HEIGHT_SIZE)
             .centerCrop()
             .into(binding.detailImg)
-
     }
 
     companion object {
         private const val IMG_WIDTH_SIZE = 1500
         private const val IMG_HEIGHT_SIZE = 1500
-        private const val ENTIRE_PAGE_NUM = 3
-        private const val CUR_PAGE_NUM = 1
-
         private const val IMAGE_DOWNLOAD_COMPLETE = "이미지 다운 완료"
     }
 }
